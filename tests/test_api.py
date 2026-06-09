d7bf17b30b30110d4962935dc04e435665d7ec14
root <root@srv2.domain.com>
2026-06-09 22:36:07 +0200

    fix pytest import path

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,10 @@
 from fastapi.testclient import TestClient
+import sys
+import os
+
+# Ajoute le dossier racine du projet au PYTHONPATH
+sys.path.append(os.path.abspath("."))
+
 from main import app
 
 client = TestClient(app)
@@ -6,4 +12,3 @@ client = TestClient(app)
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
-
